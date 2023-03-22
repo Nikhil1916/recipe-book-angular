2f12888d1c6af7cdef14a599ffaa47248a292f10
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2023-03-22 23:14:50 +0530

    fixed redirection to page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipes.service';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { AppRoutingModule } from './app-routing.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { sharedModule } from './shared/shared.module';
 import { coreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -21,10 +17,10 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    AppRoutingModule,
     sharedModule,
     coreModule,
     AuthModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
